08fdb59a98b90fa555d82c8c65fd65b749f48940
Kozyreva Olga <kozyrevaolga@gmail.com>
2025-08-04 19:18:41 +0700

    created the project structure

---
+++ tree_ok.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+
+flags = [ "--help", "--version", "-a", "-d", "-f", "-h", "-u", "-g", "-D", "-C", "-L" ]
+
+if len(sys.argv) = 0:
+  tree_ok
+elif len(sys.argv) = 1:
+  if sys.argv[1] = # check with flag_list or get the path, then go to other functions 
+elif len(sys.argv) = 2:
+  tree_ok_with_flags_and_path
+  
+  
+# functions
+
+def tree_ok();
+
+def tree_ok_with_flags():
+
+def tree_ok_with_path():
+
+def tree_ok_with_flags_and_path():
+
+def f_help():
+
+def f_version():
+
+def f_a():
+
+def f_d():
+
+def f_h():
+
+def f_u():
+
+def f_g():
+
+def f_D():
+
+def f_C():
+
+def f_L():
