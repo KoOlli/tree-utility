39b2246c153874685465cc1d9dccabad45b17db9
Kozyreva Olga <kozyrevaolga@gmail.com>
2025-08-07 16:58:30 +0700

    wrote the rendering of dirs & files correctly. also need to change the rendering of the latest files & dirs

--- test_py_f.py
+++ test_py_f.py
@@ -1,10 +1,30 @@
 import os
 
 for root, dirs, files in os.walk('.'):
-    # print(f"Директория: {root}")
-    # print(f"Поддиректории: {dirs}")
-    # print(f"Файлы: {files}")
-    levels = root.split(os.sep)  # ['.', '.git', 'hooks'] тут раздробленно пишутся названия подкаталогов соответствуя количеству уровней
-    depth = len(levels) - 1 # тут у нас числами назначаются уровни директорий
+    dirs.sort()
+    files.sort()
+    
+    levels = root.split(os.sep)
+    dir_ = root.split(os.sep)[-1]
+    depth = len(levels) - 1 
+    
+    if depth == 0:
+        print(f"{dir_}")
+        root_files = files
+        continue
+    
+    indent = '│   ' * (depth - 1)
+    prefix = '├──' 
+    print(f"{indent}{prefix} {dir_}")
 
-    print(f"Директория: {depth.sort()}")
+    for i, file in enumerate(files):
+        indentF = '│   ' * (depth)
+        last_file = (i == (len(files) - 1))
+        prefix = '└──' if last_file else '├──' 
+        print(f"{indentF}{prefix} {file}") 
+    
+for i, fIle in enumerate(root_files):
+    indentX = '│   ' * (depth - 2)
+    last_filE = (i == (len(root_files) - 1))
+    Prefix = '└──' if last_filE else '├──' 
+    print(f"{indentX}{Prefix} {fIle}") 
